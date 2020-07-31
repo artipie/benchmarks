576ea2b6a9f20bbd97dd9d29dc344b11fd3df5a1
Sammers21 <titantins@gmail.com>
2020-07-31 13:34:11 +0300

    #21 dont export private ips since we dont use them

--- aws-infrastructure/start.sh
+++ aws-infrastructure/start.sh
@@ -15,8 +15,6 @@ terraform init
 terraform apply -input=false -auto-approve
 
 # Export terraform output variables
-export PRIVATE_CLIENT_IP_ADDR=$(terraform output private_client_ip_addr)
-export PRIVATE_SERVER_IP_ADDR=$(terraform output private_server_ip_addr)
 export PUBLIC_CLIENT_IP_ADDR=$(terraform output public_client_ip_addr)
 export PUBLIC_SERVER_IP_ADDR=$(terraform output public_server_ip_addr)
 

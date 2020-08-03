654149c30eab8dc76b961e8d04c0c95d998d8eef
Sammers21 <titantins@gmail.com>
2020-08-03 10:03:39 +0300

    #21 add set-env.sh script for an easy variables import

---
+++ aws-infrastructure/set-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Enter script-located dir, whaterver the script is called from
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+export PRIVATE_CLIENT_IP_ADDR=$(terraform output private_client_ip_addr)
+export PRIVATE_SERVER_IP_ADDR=$(terraform output private_server_ip_addr)
+export PUBLIC_CLIENT_IP_ADDR=$(terraform output public_client_ip_addr)
+export PUBLIC_SERVER_IP_ADDR=$(terraform output public_server_ip_addr)

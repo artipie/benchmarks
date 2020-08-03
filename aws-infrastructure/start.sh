1127e9105983346aa8cad87382953bde14ab4cca
Sammers21 <titantins@gmail.com>
2020-08-03 10:04:46 +0300

    #21 install required software on VM's

--- aws-infrastructure/start.sh
+++ aws-infrastructure/start.sh
@@ -14,12 +14,33 @@ fi
 terraform init
 terraform apply -input=false -auto-approve
 
-# Export terraform output variables
-export PUBLIC_CLIENT_IP_ADDR=$(terraform output public_client_ip_addr)
-export PUBLIC_SERVER_IP_ADDR=$(terraform output public_server_ip_addr)
+# Setup env variables with VMm's ip
+source set-env.sh
 
 # Wait for VMs to start
 for IP in $PUBLIC_SERVER_IP_ADDR $PUBLIC_CLIENT_IP_ADDR
 do
     until timeout 30 ssh -i aws_ssh_key -oStrictHostKeyChecking=no ubuntu@$IP exit; do sleep 5 ; done
+done
+
+# Install required software on each VM
+for IP in $PUBLIC_SERVER_IP_ADDR $PUBLIC_CLIENT_IP_ADDR
+do
+ssh -i aws_ssh_key -oStrictHostKeyChecking=no ubuntu@$IP <<'ENDSSH'
+set -x
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+ENDSSH
 done

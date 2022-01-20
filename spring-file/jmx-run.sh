d7691c820b689509c99424be74b101b0275eca7d
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2022-01-20 16:17:51 +0300

    changes by review

--- spring-file/jmx-run.sh
+++ spring-file/jmx-run.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # Run performance tests for Spring Boot server locally
+set -eo pipefail
+cd "${0%/*}"
 
 echo "Remove old results"
 rm -rf spring-upload-res
-rm spring-upload.log
+rm -f spring-upload.log
 
-mkdir -p spring-upload-res
+mkdir spring-upload-res
 
 echo "Run jmeter tests"
 "PATH_TO_JMETER" jmeter -n -t ../files/upload-files.jmx -l spring-upload.log -e -o ./spring-upload-res -Jrepository.host=localhost -Jrepository.port=8080
-sleep 10
--- spring-file/spring-run.sh
+++ spring-file/spring-run.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # Start Spring Boot server
+set -eo pipefail
 
 mvn clean install
 mvn dependency:copy-dependencies
-rm rf spring.log
+rm -f spring.log
 java -cp "target/spring-file-1.0-SNAPSHOT.jar:target/classes/*:target/dependency/*" com.artipie.spring.Application

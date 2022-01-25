f92785adcf2c4b71d901c459877c084c103357bd
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2022-01-25 11:17:44 +0300

    changes by review

--- artipie-file/src/main/java/com/artipie/file/CleanSlice.java
+++ artipie-file/src/main/java/com/artipie/file/CleanSlice.java
@@ -6,16 +6,17 @@ package com.artipie.file;
 
 import com.artipie.http.Response;
 import com.artipie.http.Slice;
+import com.artipie.http.async.AsyncResponse;
 import com.artipie.http.rs.RsStatus;
 import com.artipie.http.rs.RsWithStatus;
 import com.artipie.http.rs.StandardRs;
-import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import org.apache.commons.io.FileUtils;
 import org.reactivestreams.Publisher;
 
 /**
@@ -42,16 +43,26 @@ final class CleanSlice implements Slice {
         final Iterable<Map.Entry<String, String>> headers,
         final Publisher<ByteBuffer> body
     ) {
-        Response res;
-        try {
-            Files.walk(this.toclean)
-                .sorted(Comparator.reverseOrder())
-                .map(Path::toFile)
-                .forEach(File::delete);
-            res = StandardRs.OK;
-        } catch (final IOException exc) {
-            res = new RsWithStatus(RsStatus.INTERNAL_ERROR);
-        }
-        return res;
+        return new AsyncResponse(
+            CompletableFuture.runAsync(
+                () -> {
+                    try {
+                        FileUtils.deleteDirectory(this.toclean.toFile());
+                    } catch (final IOException exc) {
+                        throw new UncheckedIOException(exc);
+                    }
+                }
+            ).handle(
+                (res, thr) -> {
+                    final Response resp;
+                    if (thr == null) {
+                        resp = StandardRs.OK;
+                    } else {
+                        resp = new RsWithStatus(RsStatus.INTERNAL_ERROR);
+                    }
+                    return resp;
+                }
+            )
+        );
     }
 }

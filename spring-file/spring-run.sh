67daa3a67cab7e787a0035ee00ab18f7ff6405bb
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2022-01-26 12:53:56 +0300

    feat: add artipie script

--- spring-file/spring-run.sh
+++ spring-file/spring-run.sh
@@ -4,5 +4,4 @@ set -eo pipefail
 
 mvn clean install
 mvn dependency:copy-dependencies
-rm -f spring.log
 java -cp "target/spring-file-1.0-SNAPSHOT.jar:target/classes/*:target/dependency/*" com.artipie.spring.Application

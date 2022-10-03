61926f057e7b4fc99135da36a050afaf32fbee7b
Evgeny Chugunnyy <chgena@mail.ru>
2022-10-03 09:15:19 +0300

    switch to env.

--- loadtests/artipie-docker.sh
+++ loadtests/artipie-docker.sh
@@ -8,5 +8,5 @@ port="$1"
 cd `dirname "$0"`
 
 docker stop artipie || :
-docker run -d --rm --name artipie -it -v "$PWD/root/etc/artipie.yaml:/etc/artipie.yml" -v "$PWD/root/var:/var/artipie"  --user 0:0 -p "$port":8080 -p 8086:8086 -p 8888:8888 artipie/artipie:1.0-SNAPSHOT --jmx
+docker run -d --rm --name artipie -it -v "$PWD/root/etc/artipie.yaml:/etc/artipie.yml" -v "$PWD/root/var:/var/artipie"  --user 0:0 -p "$port":8080 -p 8086:8086 -p 8888:8888 -e JMX_PORT=8888 artipie/artipie:1.0-SNAPSHOT
 

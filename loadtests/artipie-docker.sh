1bb7bdcbf6a3cc806da6f23bbd736ecb96f565ab
Evgeny Chugunnyy <chgena@mail.ru>
2022-10-25 09:03:17 +0300

    Test scripts

--- loadtests/artipie-docker.sh
+++ loadtests/artipie-docker.sh
@@ -5,7 +5,8 @@ if [ $# -ne 1 ]; then
 fi
 port="$1"
 JMX_PORT=9999
+JDEBUG=8000
 cd `dirname "$0"`
 
 docker stop artipie || :
-docker run -d --rm --name artipie -it -v "$PWD/root/etc/artipie.yaml:/etc/artipie.yml" -v "$PWD/root/var:/var/artipie"  --user 0:0 -p "$port":8080 -p 8086:8086 -p $JMX_PORT:$JMX_PORT -e JVM_ARGS="-Djava.rmi.server.hostname=localhost -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" chgena/artipie #artipie/artipie:1.0-SNAPSHOT
+docker run -d --rm --name artipie -it -v "$PWD/root/etc/artipie.yaml:/etc/artipie.yml" -v "$PWD/root/var:/var/artipie"  --user 0:0 -p "$port":8080 -p 8086:8086 -p $JMX_PORT:$JMX_PORT -p$JDEBUG:$JDEBUG -e JVM_ARGS="-agentlib:jdwp=transport=dt_socket,server=y,address=*:$JDEBUG,suspend=n -Djava.rmi.server.hostname=localhost -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" artipie/artipie:1.0-SNAPSHOT

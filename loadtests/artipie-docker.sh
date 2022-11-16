418619d7f0bf77c29867cc61bd1725e0b9964871
Evgeny Ch <chgena@mail.ru>
2022-11-16 14:08:11 +0300

    java agent test

--- loadtests/artipie-docker.sh
+++ loadtests/artipie-docker.sh
@@ -6,7 +6,8 @@ fi
 port="$1"
 JMX_PORT=9999
 JDEBUG=8000
+JMX_AGENT=12345
 cd `dirname "$0"`
 
 docker stop artipie || :
-docker run --pull=missing -d --rm --name artipie -it -v "$PWD/root/etc/artipie.yaml:/etc/artipie.yml" -v "$PWD/root/var:/var/artipie"  --user 0:0 -p "$port":8080 -p 8086:8086 -p $JMX_PORT:$JMX_PORT -p$JDEBUG:$JDEBUG -e JVM_ARGS="-agentlib:jdwp=transport=dt_socket,server=y,address=*:$JDEBUG,suspend=n -Djava.rmi.server.hostname=localhost -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" artipie/artipie:v0.27.4
+docker run --pull=missing -d --rm --name artipie -it -v "$PWD/root/etc:/etc" -v "$PWD/root/var:/var/artipie"  --user 0:0 -p "$port":8080 -p 8086:8086 -p $JMX_AGENT:$JMX_AGENT -p $JMX_PORT:$JMX_PORT -p$JDEBUG:$JDEBUG -e JVM_ARGS="-javaagent:/etc/jmx_prometheus_javaagent-0.17.2.jar=$JMX_AGENT:/etc/config-agent.yaml -agentlib:jdwp=transport=dt_socket,server=y,address=*:$JDEBUG,suspend=n -Djava.rmi.server.hostname=localhost -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" artipie/artipie:v0.27.4
